70b2d550a10145e25cb1cd5cdba6476a05a55031
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-05-05 10:07:28 +0200

    Lagt till cors i server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', authRoutes)
